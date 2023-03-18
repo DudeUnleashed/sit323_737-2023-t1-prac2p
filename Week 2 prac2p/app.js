80df0a0be06a075de7608a92714c9ab4380885bf
Jake Mansfield <jmansfield0209@gmail.com>
2023-03-18 15:51:20 +1100

    Edits to workshop 2

--- Week 2 prac2p/app.js
+++ Week 2 prac2p/app.js
@@ -9,8 +9,29 @@ let users = [
     {id: 3, name: 'Bob'}
 ];
 
+app.get('/', (req, res) => {        
+    res.send("Welcome to our server!!");
+}); 
+
 app.get('/users', (req, res) => {
     res.json(users);
+});
+
+app.get('/users/:id', function(req, res) {
+    console.log("User ID " + req.params.id + " requested");
+    var userID = req.params.id;
+    var userFound = false;
+
+    users.forEach((user, index, array) => {
+        if (user.id == userID) {
+            res.send(users[index]);
+            userFound = true;
+        }
+    });
+
+    if (userFound == false) {
+        res.send("ERROR: User with ID " + userID + " does not exist");
+    }
 });
 
 app.get('/users/:id', (req, res) => {
@@ -29,6 +50,7 @@ app.post('/users', jsonParser, (req, res) => {
 
 app.put('/users/:id', jsonParser, (req, res) => {
     const userId = parseInt(req.params.id);
+    console.log("Update user with ID: " + req.params.id);
     const updatedUser = req.body;
     users = users.map(user => user.id === userId ? updatedUser : user);
     res.status(200).json(updatedUser);
