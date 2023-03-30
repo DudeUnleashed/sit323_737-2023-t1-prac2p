f98e010b4da907c9915c6a6891c5ed4f932d1d57
Jake Mansfield <jmansfield0209@gmail.com>
2023-03-30 11:03:35 +1100

    updating calculator

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,44 +1,109 @@
 const express = require("express")
 const app = express();
 
+const add = (n1,n2) => {
+    return n1 + n2;
+}
+const subtract = (n1,n2) => {
+    return n1 - n2;
+}
+const divide = (n1,n2) => {
+    return n1 / n2;
+}
+const multiply = (n1,n2) => {
+    return n1 * n2;
+}
+
 app.get('/', (req, res) => {        
     res.send("Welcome to our server!!");
 }); 
 
-app.get('/addition/:num1/:num2', (req, res) => {
-    num1 = req.params.num1;
-    num2 = req.params.num2;
-    if (isNaN(num1) || isNaN(num2)) {
-        res.send("Please enter a valid number");
+app.get('/add', (req, res) => {
+    try {
+        n1 = parseFloat(req.query.n1);
+        n2 = parseFloat(req.query.n2);
+        if (isNaN(n1)) {
+            throw new Error("n1 is incorrectly defined");
+        }
+        if (isNaN(n2)) {
+            throw new Error("n2 is incorrectly defined");
+        }
+        if (n1 === NaN || n2 === NaN){
+            console.log()
+            throw new Error("Parsing Error");
+        }
+        const result = add(n1,n2);
+        res.status(200).json({statuscocde:200, data: result});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
     }
-    res.send(`The sum of ${num1} and ${num2} is ${parseInt(num1) + parseInt(num2)}`);
 });
 
-app.get('/subtraction/:num1/:num2', function(req, res) {
-    num1 = req.params.num1;
-    num2 = req.params.num2;
-    if (isNaN(num1) || isNaN(num2)) {
-        res.send("Please enter a valid number");
+app.get('/subtract', function(req, res) {
+    try {
+        n1 = parseFloat(req.query.n1);
+        n2 = parseFloat(req.query.n2);
+        if (isNaN(n1)) {
+            throw new Error("n1 is incorrectly defined");
+        }
+        if (isNaN(n2)) {
+            throw new Error("n2 is incorrectly defined");
+        }
+        if (n1 === NaN || n2 === NaN){
+            console.log()
+            throw new Error("Parsing Error");
+        }
+        const result = subtract(n1,n2);
+        res.status(200).json({statuscocde:200, data: result});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
     }
-    res.send(`The sum of ${num1} and ${num2} is ${parseInt(num1) - parseInt(num2)}`);
 });
 
-app.get('/division/:num1/:num2', (req, res) => {
-    num1 = req.params.num1;
-    num2 = req.params.num2;
-    if (isNaN(num1) || isNaN(num2)) {
-        res.send("Please enter a valid number");
+app.get('/divide', (req, res) => {
+    try {
+        n1 = parseFloat(req.query.n1);
+        n2 = parseFloat(req.query.n2);
+        if (isNaN(n1)) {
+            throw new Error("n1 is incorrectly defined");
+        }
+        if (isNaN(n2)) {
+            throw new Error("n2 is incorrectly defined");
+        }
+        if (n1 === NaN || n2 === NaN){
+            console.log()
+            throw new Error("Parsing Error");
+        }
+        const result = divide(n1,n2);
+        res.status(200).json({statuscocde:200, data: result});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
     }
-    res.send(`The sum of ${num1} and ${num2} is ${parseInt(num1) / parseInt(num2)}`);
 });
 
-app.get('/multiplication/:num1/:num2', (req, res) => {
-    num1 = req.params.num1;
-    num2 = req.params.num2;
-    if (isNaN(num1) || isNaN(num2)) {
-        res.send("Please enter a valid number");
+app.get('/multiply', (req, res) => {
+    try {
+        n1 = parseFloat(req.query.n1);
+        n2 = parseFloat(req.query.n2);
+        if (isNaN(n1)) {
+            throw new Error("n1 is incorrectly defined");
+        }
+        if (isNaN(n2)) {
+            throw new Error("n2 is incorrectly defined");
+        }
+        if (n1 === NaN || n2 === NaN){
+            console.log()
+            throw new Error("Parsing Error");
+        }
+        const result = multiply(n1,n2);
+        res.status(200).json({statuscocde:200, data: result});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
     }
-    res.send(`The sum of ${num1} and ${num2} is ${parseInt(num1) * parseInt(num2)}`);
 });
 
 app.listen(3000, () => {
